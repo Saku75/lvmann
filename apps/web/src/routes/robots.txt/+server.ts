4372a2d2676a05fa2950eafc4018691ddc932172
Lukas Vendel Mann <lukas@lvmann.io>
2025-03-07 17:45:43 +0000

    Implement sitemap.xml and robots.txt

---
+++ apps/web/src/routes/robots.txt/+server.ts
@@ -0,0 +1,18 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ url }) => {
+  const body = [
+    "User-agent: *",
+    "Allow: /",
+    "",
+    `Sitemap: ${url.origin}/sitemap.xml`,
+  ]
+    .join("\n")
+    .trim();
+
+  const headers = {
+    "Content-Type": "text/plain",
+  };
+
+  return new Response(body, { headers });
+};
