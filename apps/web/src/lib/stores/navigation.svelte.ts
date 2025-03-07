bce71e63e61218311aaff8b731ebe323f71eeb55
Lukas Vendel Mann <lukas@lvmann.io>
2025-03-07 21:20:22 +0000

    Update icons, heading font and navigation layout

--- apps/web/src/lib/stores/navigation.svelte.ts
+++ apps/web/src/lib/stores/navigation.svelte.ts
@@ -1,10 +1,11 @@
-import type { IconEvents, IconProps } from "lucide-svelte";
-import type { SvelteComponent } from "svelte";
+import type { Icon } from "@lucide/svelte";
+import House from "@lucide/svelte/icons/house";
+import Library from "@lucide/svelte/icons/library";
 
 type NavigationItem = {
   text: string;
   href: string;
-  Icon?: typeof SvelteComponent<IconProps, IconEvents, object>;
+  Icon?: typeof Icon;
 };
 
 type NavigationStore =
@@ -13,9 +14,10 @@ type NavigationStore =
 
 const navigationStore = $state<NavigationStore>([
   [
-    { text: "Home", href: "/" },
-    { text: "Blog", href: "/blog" },
+    { text: "Home", href: "/", Icon: House },
+    { text: "Blog", href: "/blog", Icon: Library },
   ],
+  [{ text: "Github", href: "https://github.com/Saku75" }],
 ]);
 
 export { navigationStore };
