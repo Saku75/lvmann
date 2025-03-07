bdf52439e45a2378d205ca50884d36e0ec0f87d7
Lukas Vendel Mann <lukas@lvmann.io>
2025-03-07 14:46:54 +0000

    Implement metadata handling

---
+++ apps/web/src/lib/components/layout/footer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Copyright } from "lucide-svelte";
+
+  import CenterContent from "../utils/center-content.svelte";
+</script>
+
+<CenterContent class="flex h-6 justify-end px-2">
+  <small
+    class="flex items-center gap-1 text-xs text-stone-600 dark:text-stone-400"
+  >
+    <Copyright class="h-3 w-3" />
+    <span>Lukas Vendel Mann - {new Date().getFullYear()}</span>
+  </small>
+</CenterContent>
