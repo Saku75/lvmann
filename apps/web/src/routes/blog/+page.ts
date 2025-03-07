72e8502062356d0df2e2dc398a45883d51982bd0
Lukas Vendel Mann <lukas@lvmann.io>
2025-03-07 13:38:56 +0000

    Add layout components and navigation structure with responsive design

--- apps/web/src/routes/blog/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getPosts } from "$lib/posts";
-
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async () => {
-  const posts = getPosts();
-
-  return { posts };
-};
