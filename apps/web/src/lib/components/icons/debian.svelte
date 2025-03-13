e50280686862222afdb6821e83efc5f8c6e43d7b
Lukas Vendel Mann <lukas@lvmann.io>
2025-03-13 14:17:08 +0000

    Implement more custom icons

---
+++ apps/web/src/lib/components/icons/debian.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Icon, type IconProps } from "@lucide/svelte";
+  import { siDebian } from "simple-icons";
+
+  import svgToIconNode from "./utils/svg-to-icon-node";
+
+  const {
+    name,
+    color = "currentColor",
+    size = 24,
+    strokeWidth = 2,
+    absoluteStrokeWidth = false,
+    ...props
+  }: Omit<IconProps, "iconNode" | "children"> = $props();
+</script>
+
+<Icon
+  {...props}
+  iconNode={svgToIconNode(siDebian.svg)}
+  strokeWidth="0"
+  fill={color}
+/>
---
+++ apps/web/src/lib/components/icons/dotnet.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Icon, type IconProps } from "@lucide/svelte";
+  import { siDotnet } from "simple-icons";
+
+  import svgToIconNode from "./utils/svg-to-icon-node";
+
+  const {
+    name,
+    color = "currentColor",
+    size = 24,
+    strokeWidth = 2,
+    absoluteStrokeWidth = false,
+    ...props
+  }: Omit<IconProps, "iconNode" | "children"> = $props();
+</script>
+
+<Icon
+  {...props}
+  iconNode={svgToIconNode(siDotnet.svg)}
+  strokeWidth="0"
+  fill={color}
+/>
