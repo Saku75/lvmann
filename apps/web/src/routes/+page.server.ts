f0fa419853d36533a334859dc608c4467bd9a3a5
Lukas Vendel Mann <lukas@lvmann.io>
2025-03-27 17:21:33 +0000

    Focus website for production version

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -5,7 +5,7 @@ export const load: PageServerLoad = async () => {
     metadata: {
       title: ["Lukas Vendel Mann", "Software developer"],
       description:
-        "I'm Lukas Vendel Mann, a software developer passionate about building user- and performance-oriented apps, distributed systems, and scalable software. Explore my projects, check out my blog, or get in touch—let's build something great!",
+        "I'm Lukas Vendel Mann, a software developer passionate about building user- and performance-oriented apps, distributed systems, and scalable software. Explore my projects on Github, or get in touch—let's build something great!",
       author: "Lukas Vendel Mann",
     },
   };
